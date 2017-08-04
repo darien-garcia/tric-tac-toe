df1dd27076d4ca08f57a635877ea433535d16026
kleer <kleer@kleer-csd-box>
2017-08-04 10:33:21 -0500

    se agrega pantalla con numero de celdas disponibles

--- features/step_definitions/my_steps.rb
+++ features/step_definitions/my_steps.rb
@@ -10,3 +10,14 @@ Then(/^debo ver "([^"]*)"$/) do |texto|
   expect(page.body).to match /#{texto}/m
 end
 
+Given(/^que comenzo el juego$/) do
+  step "que abri el juego"
+  step "inicie la partida"
+end
+
+#Then(/^debo ver una casilla disponible$/) do 
+#	expect(page.should  have_selector("input[type=submit][value='_']")).to be(true)
+#end
+
+
+
