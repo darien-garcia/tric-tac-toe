8da54c88664008fb9e49ff3bb3358e4be5751e98
rickirea <rickirea@gmail.com>
2017-08-04 12:31:17 -0500

    corregir pruebas cambio de jugador

--- app.rb
+++ app.rb
@@ -8,20 +8,18 @@ end
 
 get '/jugar' do
 	session['juego'] = Juego.new
-	session['simbolo'] = 'X'
+	session['simbolo'] = '-'
 	erb :jugar
 end
 
 post '/jugada' do
-	
-	session['juego'].jugada :a1 
 
 	if session['juego'].siguiente == :p1
-		session['juego'].jugada :a2
-
-		session['simbolo'] = 'O'
-	else
 		session['simbolo'] = 'X'
+		session['juego'].jugada :a2
+	else
+		session['simbolo'] = 'O'
+		session['juego'].jugada :a1
 	end
 
 
