3c1249f7834d2f31bf7933b1ea6d0ce19aa78fab
darien-garcia <dgarcia@itexico.net>
2017-08-04 10:34:34 -0500

    cambio de turno

--- features/step_definitions/my_steps.rb
+++ features/step_definitions/my_steps.rb
@@ -10,3 +10,20 @@ Then(/^debo ver "([^"]*)"$/) do |texto|
   expect(page.body).to match /#{texto}/m
 end
 
+Given(/^que hay una partida iniciada$/) do
+  step 'que abri el juego'
+  step 'inicie la partida'
+end
+
+
+
+Given(/^que es turno del jugador "([^"]*)"$/) do |jugador|
+  expect(page.body).to match "siguiente jugador: #{jugador}"
+end
+
+Given(/^que el jugador en turno jugo$/) do
+  click_button('play')
+end
+
+
+
