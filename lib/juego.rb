d3d1ebcedeedce76481bbf9d6c9c2f442ff4cf9f
darien-garcia <dgarcia@itexico.net>
2017-08-04 11:53:23 -0500

    agregamos matriz de juego en el modelo de juego

--- lib/juego.rb
+++ lib/juego.rb
@@ -1,16 +1,32 @@
 class Juego
+
 	def initialize 
 		@siguiente = :p1
+		@tablero = {:a1=>:vacio,:a2=>:vacio,:a3=>:vacio,:b1=>:vacio,:b2=>:vacio,:b3=>:vacio,:c1=>:vacio,:c2=>:vacio,:c3=>:vacio}
+		@simbolos = {:p1=>"X", :p2=>"O", :vacio=>"_"}
 	end
 	def siguiente 
 		@siguiente
 	end
 
-	def jugada 
+	def jugada eleccion
+
+		if vacia? eleccion
+			@tablero[eleccion] = @simbolos[@siguiente]
+		else
+			false
+		end
+
 		if @siguiente == :p1
 			@siguiente = :p2
 		else
 			@siguiente = :p1
 		end
+
+		return true
+	end
+
+	def vacia? eleccion
+		return @tablero[eleccion].to_sym == :vacio
 	end
 end
--- spec/tric_tac_toe_spec.rb
+++ spec/tric_tac_toe_spec.rb
@@ -6,13 +6,23 @@ describe Juego do
 		expect(juego.siguiente).to eq :p1
 	end
 
-	it "siguiente es p2 si juega p1" do
+	it "siguiente es p2 si juega p1 eligiendo la celda :a1" do
 		juego = Juego.new
-		juego.jugada
+		juego.jugada :a1
 		expect(juego.siguiente).to eq :p2
 	end
 
-	it "siguiente es p1 si juega p2" do
-		
+	it "siguiente es p1 si juega p2  eligiendo la celda :a2" do
+		juego = Juego.new
+		juego.jugada :a1
+		juego.jugada :a2
+		expect(juego.siguiente).to eq :p1
 	end
+
+	it "jugador p1 selecciona la casilla :a1" do
+		juego = Juego.new
+		valido = juego.jugada :a1
+		expect(valido) ==  true
+	end
+
 end
